36654574badd233ab40be7ab6d115d2f7d0d40a8
Simon <simonbertron@gmail.com>
2025-01-29 17:09:04 -0500

    LESSON 1: add statistical breakdown to analyzer tool
    This is my "do any other thing" from the lesson 1 description.
    
    Also get overly excited about graphviz and modify cfg printing to
    cluster basic blocks by function.

--- l1/analyzer.py
+++ l1/analyzer.py
@@ -88,15 +88,69 @@ def construct_cfg(labels_and_blocks):
 
 
 # TODO: we could produce a graphviz data structure in Python and skip the text
-def print_cfg(cfg):
+def print_cfg_in_dot(cfg, labels_and_blocks):
+    cluster_number = 0
+
     print("digraph cfg {")
-    for label in cfg.keys():
-        print(f'"{label}"')
-    print()
+
+    for func, l_and_b in labels_and_blocks.items():
+        print(f"  subgraph cluster{cluster_number} {{")
+        print("    style = filled;")
+        print("    color = lightgrey;")
+        print("    node [style=filled,color=white];")
+        print(f'    label = "@{func}";')
+        for label, block in l_and_b:
+            print(f'    "{label}";')
+        print("  }")
+        print()
+        cluster_number += 1
+
     for label, dests in cfg.items():
         for dest in dests:
-            print(f'"{label}" -> "{dest}"')
+            print(f'  "{label}" -> "{dest}";')
+
     print("}")
+
+
+def print_cfg(cfg):
+    for label in cfg.keys():
+        print(f"{label}")
+
+    print()
+
+    for label, dests in cfg.items():
+        for dest in dests:
+            print(f"{label} -> {dest}")
+
+
+def print_toplevel_counts(prog):
+    print(f"Number of functions: {len(prog['functions'])}")
+    label_count = 0
+    call_count = 0
+    for func in prog["functions"]:
+        for instr in func["instrs"]:
+            if "label" in instr:
+                label_count += 1
+            elif instr["op"] == "call":
+                call_count += 1
+    print(f"Number of labels: {label_count}")
+    print(f"Number of function call sites: {call_count}")
+
+
+def print_opcode_breakdown(prog):
+    opcodes = {}
+    for func in prog["functions"]:
+        for instr in func["instrs"]:
+            if "op" in instr:
+                if instr["op"] not in opcodes:
+                    opcodes[instr["op"]] = 0
+                opcodes[instr["op"]] += 1
+    total = sum(opcodes.values())
+
+    indent = 1 + max(map(len, opcodes.keys()))
+    print("Breakdown of instructions by opcode:")
+    for opcode, count in opcodes.items():
+        print(f"  {opcode}:{' '*(indent-len(opcode))}{round(count/total*100)}%")
 
 
 @click.group
@@ -113,11 +167,26 @@ def blocks():
 
 
 @main.command
-def cfg():
+@click.option("--dot", is_flag=True, help="print out cfg in graphviz dot language")
+def cfg(dot):
     """
-    Construct control flow graph of a bril program, labelled by labels from block command (printed out in graphviz dot language)
+    Construct control flow graph of a bril program, labelled by labels from block command
     """
-    print_cfg(construct_cfg(parse_into_blocks(json.load(sys.stdin))))
+    labels_and_blocks = parse_into_blocks(json.load(sys.stdin))
+    cfg = construct_cfg(labels_and_blocks)
+    if dot:
+        print_cfg_in_dot(cfg, labels_and_blocks)
+    else:
+        print_cfg(cfg)
+
+
+@main.command
+@click.option("--dot", is_flag=True, help="print out cfg in graphviz dot language")
+def fcg():
+    """
+    Construct function call graph of a bril program
+    """
+    print("TODO")
 
 
 @main.command
@@ -125,7 +194,9 @@ def stats():
     """
     Calculate and print various statistics about a bril program
     """
-    pass
+    prog = json.load(sys.stdin)
+    print_toplevel_counts(prog)
+    print_opcode_breakdown(prog)
 
 
 if __name__ == "__main__":
