cf9231d53f618007d77c35f61476d0f4ebca190b
Simon <simonbertron@gmail.com>
2025-02-04 17:56:17 -0500

    UTIL: add an initial utilities library
    So far it only contains functions for converting into and out of a
    basic block centric representation.

---
+++ utilities.py
@@ -0,0 +1,42 @@
+import json
+import sys
+
+
+def parse_into_blocks(prog):
+    all_blocks = {}
+    label_number = -1
+    for f in prog["functions"]:
+        blocks = []
+        label_number += 1
+        label = f"b{label_number}"
+        cur = []
+        for i in f["instrs"]:
+            if "label" in i:
+                if len(cur) != 0:
+                    blocks.append((label, cur))
+                cur = [i]
+                label = i["label"]
+            elif i["op"] in ("jmp", "br", "ret"):
+                cur.append(i)
+                blocks.append((label, cur))
+                cur = []
+                label_number += 1
+                label = f"b{label_number}"
+            else:
+                cur.append(i)
+        if len(cur) != 0:
+            blocks.append((label, cur))
+        all_blocks[f["name"]] = blocks
+    return all_blocks
+
+
+def coalesce_blocks(blocks):
+    json_dict = {"functions": []}
+    for func in blocks:
+        func_dict = {"name": func}
+        instrs = []
+        for _, block in blocks[func]:
+            instrs += block
+        func_dict[instrs] = instrs
+        json_dict["functions"].append(func_dict)
+    return json_dict
