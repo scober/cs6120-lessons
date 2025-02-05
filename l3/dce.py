83a73d15e51c046c95c7d15a81dcc52a5f87c3b5
Simon <simonbertron@gmail.com>
2025-02-05 13:46:41 -0500

    LESSON 3: local dead code elimination
    Within a basic block, remove instructions that assign to variables that
    are not read before being assigned to again.

--- l3/dce.py
+++ l3/dce.py
@@ -20,6 +20,27 @@ def global_simple_dead_code_elimination(instrs):
     return out
 
 
+@ut.local_optimization
+@ut.iterate_to_convergence
+def local_simple_dead_code_elimination(block):
+    out = []
+    assigned_before_next_use = set()
+    for instr in reversed(block):
+        if (
+            "dest" in instr
+            and instr["dest"] in assigned_before_next_use
+            and not ut.has_side_effects(instr)
+        ):
+            continue
+        out.insert(0, instr)
+        if "dest" in instr:
+            assigned_before_next_use.add(instr["dest"])
+        if "args" in instr:
+            for arg in instr["args"]:
+                assigned_before_next_use.discard(arg)
+    return out
+
+
 @click.group
 def main():
     pass
@@ -28,6 +49,11 @@ def main():
 @main.command(name="global")
 def global_dce():
     print(json.dumps(global_simple_dead_code_elimination(json.load(sys.stdin))))
+
+
+@main.command(name="local")
+def local_dce():
+    print(json.dumps(local_simple_dead_code_elimination(json.load(sys.stdin))))
 
 
 if __name__ == "__main__":
