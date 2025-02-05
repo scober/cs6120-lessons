7a7807fd696c698101e9353723e2234b5172728f
Simon <simonbertron@gmail.com>
2025-02-04 19:46:30 -0500

    TESTING: add an optimization tester
    Given an optimization pass and some number of bril programs, check that
    the optimization does not change the behavior of the program or affect
    its performance negatively.

---
+++ optimized.py
@@ -0,0 +1,103 @@
+import json
+import pprint
+import subprocess
+
+import click
+from colored import Fore, Back, Style
+
+import utilities as ut
+
+
+def run_bril(filename, optimizer):
+    assert filename.endswith(".bril"), f"{filename} does not end with .bril"
+
+    args = ""
+    with open(filename, "r") as f:
+        for line in f:
+            if "ARGS" in line:
+                args = line.split(":")[-1].strip()
+
+    pipeline = f"bril2json < {filename} | {optimizer} | brili -p {args}"
+    output = subprocess.getoutput(pipeline).splitlines()
+    assert output[-1].startswith("total_dyn_inst:"), f"{pipeline} gave output {output}"
+
+    return output[:-1], output[-1].split(" ")[-1]
+
+
+def count_static_instructions(filename, optimizer):
+    count = 0
+    prog = json.loads(subprocess.getoutput(f"bril2json < {filename} | {optimizer}"))
+    for func in prog["functions"]:
+        for instr in func["instrs"]:
+            if "label" not in instr:
+                count += 1
+
+    return count
+
+
+def check_output(bril, optimizer):
+    unoptimized_output, _ = run_bril(bril, "cat")
+    optimized_output, _ = run_bril(bril, optimizer)
+
+    assert (
+        unoptimized_output == optimized_output
+    ), f"optimization {optimizer} broke file {bril}, turned {unoptimized_output} into {optimized_output}"
+
+
+def check_static_instructions(bril, optimizer):
+    unoptimized_static = count_static_instructions(bril, "cat")
+    optimized_static = count_static_instructions(bril, optimizer)
+
+    assert (
+        unoptimized_static >= optimized_static
+    ), f"optimization {optimizer} increased the number of static instructions in {bril}, from {unoptimized_static} to {optimized_static}"
+
+
+def check_dynamic_instructions(bril, optimizer):
+    _, unoptimized_dynamic = run_bril(bril, "cat")
+    _, optimized_dynamic = run_bril(bril, optimizer)
+
+    assert (
+        unoptimized_dynamic >= optimized_dynamic
+    ), f"optimization {optimizer} increased the number of dynamic instructions in {bril}, from {unoptimized_dynamic} to {optimized_dynamic}"
+
+
+def progress():
+    print(f"{Fore.green}.{Style.reset}", end="", flush=True)
+
+
+@click.group
+def main():
+    pass
+
+
+@main.command
+@click.argument("optimizer", nargs=1)
+@click.argument("brils", nargs=-1)
+def static(optimizer, brils):
+    """
+    Ensure that the optimization has not increased the number of static static instructions
+    """
+    for bril in brils:
+        check_output(bril, optimizer)
+        check_static_instructions(bril, optimizer)
+        progress()
+    print()
+
+
+@main.command
+@click.argument("optimizer", nargs=1)
+@click.argument("brils", nargs=-1)
+def dynamic(optimizer, brils):
+    """
+    Ensure that the optimization has not increased the number of dynamic instructions
+    """
+    for bril in brils:
+        check_output(bril, optimizer)
+        check_dynamic_instructions(bril, optimizer)
+        progress()
+    print()
+
+
+if __name__ == "__main__":
+    main()
