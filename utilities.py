d1a7157688f02a04d68e45d9a9d7369c3cff3c54
Simon <simonbertron@gmail.com>
2025-02-24 09:53:57 -0500

    UTIL: move data flow framework to util
    Along the way, implement post-order and reverse-post-order traversal for
    the data flow framework and work out a bunch of bugs

--- utilities.py
+++ utilities.py
@@ -1,5 +1,7 @@
+import collections
 import copy
 import json
+import pprint
 import sys
 
 
@@ -104,3 +106,128 @@ def iterate_to_convergence(optimization_pass):
         return new
 
     return outer
+
+
+def post_order(successors):
+    # because this is a generator, we don't want to be sensitive to
+    #   changes to the input during our runtime
+    s = copy.deepcopy(successors)
+    frontier = [
+        block for block in s.keys() if all(block not in succs for succs in s.values())
+    ]
+    unvisited = [block for block in s.keys()]
+    while len(frontier):
+        block = frontier.pop(0)
+        unvisited.remove(block) if block in unvisited else None
+        frontier += [
+            succ
+            for succ in successors[block]
+            if succ not in frontier and succ in unvisited
+        ]
+        if all(block not in s[f] for f in unvisited):
+            yield block
+        else:
+            frontier.append(block)
+
+
+# this is not really worth putting in a function, but one could hypothetically
+#   calculate this more efficiently by doing it directly via the predecessors
+#   graph and putting this in its own function is my nod to that fact -- maybe
+#   one day I will do it that way
+def reverse_post_order(successors):
+    return reversed(list(post_order(successors)))
+
+
+def data_flow_analysis(
+    ret,
+    worklist,
+    successors,
+    predecessors,
+    labels_to_blocks,
+    i,
+    o,
+    merge,
+    transfer,
+):
+    # this function is written as if it is a backward pass,
+    #   but it should work just as well for a forward pass
+    while len(worklist):
+        label, block = worklist.popleft()
+
+        ins = tuple(ret[inpt][i] for inpt in successors[label])
+        ret[label][o] = merge(ins)
+
+        in_before = ret[label][i]
+        in_after = transfer(copy.deepcopy(ret[label][o]), copy.deepcopy(block))
+        ret[label][i] = in_after
+        if in_before != in_after:
+            for pred in predecessors[label]:
+                worklist.append((pred, labels_to_blocks[pred]))
+
+    return ret
+
+
+def data_flow_prerequisites(prog, entry_init, general_init):
+    blocks = parse_into_blocks(prog)
+    successors = construct_cfg(blocks)
+    predecessors = {label: [] for label in successors.keys()}
+    for pred, succs in successors.items():
+        for succ in succs:
+            predecessors[succ].append(pred)
+    labels_to_blocks = {
+        label: block for l_and_b in blocks.values() for label, block in l_and_b
+    }
+
+    ret = {
+        label: {"in": general_init(), "out": general_init()}
+        for l_and_b in blocks.values()
+        for label, _ in l_and_b
+    }
+    for label in ret:
+        if not predecessors[label]:
+            ret[label]["in"] = entry_init()
+
+    return blocks, successors, predecessors, labels_to_blocks, ret
+
+
+def forward_data_flow_analysis(prog, entry_init, general_init, merge, transfer):
+    blocks, successors, predecessors, labels_to_blocks, ret = data_flow_prerequisites(
+        prog, entry_init, general_init
+    )
+
+    worklist = collections.deque(
+        (l, labels_to_blocks[l]) for l in post_order(successors)
+    )
+
+    return data_flow_analysis(
+        ret,
+        worklist,
+        predecessors,
+        successors,
+        labels_to_blocks,
+        "out",
+        "in",
+        merge,
+        transfer,
+    )
+
+
+def backward_data_flow_analysis(prog, entry_init, general_init, merge, transfer):
+    blocks, successors, predecessors, labels_to_blocks, ret = data_flow_prerequisites(
+        prog, entry_init, general_init
+    )
+    worklist = collections.deque(
+        (l, labels_to_blocks[l]) for l in reverse_post_order(successors)
+    )
+
+    return data_flow_analysis(
+        ret,
+        worklist,
+        successors,
+        predecessors,
+        labels_to_blocks,
+        "in",
+        "out",
+        merge,
+        transfer,
+    )
