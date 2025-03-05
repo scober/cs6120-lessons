c20452708afff9d2c946df07cce5ad649fcf2b84
Simon <simonbertron@gmail.com>
2025-03-05 12:37:30 -0500

    L6: first pass at phi pass
    My initial plan to formulate the SSA transformation as standard compiler
    passes was not long for this world. Both the phi-adding and the
    variable-renaming want to traverse the CFG in a specific order.
    
    So add the skeleton of a proper SSA transformation, implement the
    phi/upsilon pass, and start implementing the variable renaming pass.

--- l6/ssa.py
+++ l6/ssa.py
@@ -1,12 +1,97 @@
 import utilities as ut
 
+import collections
 import copy
+import functools
 import json
 import pprint
-import collections
 import sys
 
 import click
+
+
+def is_defined(var, block):
+    return any(instr.get("dest", "") == var for instr in block)
+
+
+def phis(block):
+    for instr in block:
+        if instr.get("op", "") == "get":
+            yield instr
+
+
+def already_phied(var, block):
+    return any(phi["dest"] == var for phi in phis(block))
+
+
+def grab_type(var, block):
+    for instr in block:
+        if instr.get("dest", "") == var:
+            return instr["type"]
+
+
+def append_upsilon(block, var, tipe):
+    index = len(block)
+    if block[-1].get("op", "") in ["jmp", "br"]:
+        index -= 1
+    block.insert(index, {"op": "set", "type": tipe, "args": [var, var]})
+
+
+def phiify(labels_to_blocks, succs, all_vars, frontiers):
+    for var in all_vars:
+        definitions = [
+            l_and_b
+            for l_and_b in labels_to_blocks.items()
+            if is_defined(var, l_and_b[1])
+        ]
+        i = 0
+        while i < len(definitions):
+            label, block = definitions[i]
+            tipe = grab_type(var, block)
+            if len(succs[label]) != 0:
+                append_upsilon(block, var, tipe)
+            for l in frontiers[label]:
+                dominated = labels_to_blocks[l]
+                if not already_phied(var, dominated):
+                    dominated.insert(1, {"op": "get", "type": tipe, "dest": var})
+                if (l, dominated) not in definitions:
+                    definitions.append((l, dominated))
+            i += 1
+
+
+def rename_vars(var_stack, labels_to_blocks, succs, label):
+    block = labels_to_blocks[label]
+    for instr in block:
+        if "args" in instr:
+            instr["args"] = [
+                f"{arg}{'.' + str(var_stack[arg]) if var_stack[arg] else ''}"
+                for arg in instr["args"]
+            ]
+        if "dest" in instr:
+            dest = instr["dest"]
+            var_stack[dest] += 1
+            instr["dest"] = f"{dest}.{var_stack[dest]}"
+        for sl in succs[label]:
+            succ = labels_to_blocks[sl]
+            for phi in phis(succ):
+                # TODO
+                pass
+
+
+@ut.global_optimization
+def to_ssa_form(instrs):
+    prog = {"functions": [{"instrs": instrs, "name": "dummy"}]}
+    blocks, succs, preds, labels_to_blocks, entry_blocks = ut.the_stuff(prog)
+    frontiers = ut.dominator_frontiers(ut.dominators(prog), preds)
+    all_vars = ut.all_vars_in_prog(prog)
+
+    phiify(labels_to_blocks, succs, all_vars, frontiers)
+
+    assert len(entry_blocks) == 1, str(entry_blocks)
+    rename_vars({var: 0 for var in all_vars}, labels_to_blocks, succs, entry_blocks[0])
+
+    # I wish Python just had a function called fold
+    return functools.reduce(lambda acc, tup: acc + tup[1], blocks["dummy"], [])
 
 
 @ut.local_optimization
@@ -56,7 +141,7 @@ def into_ssa_command():
     """
     Convert input program into SSA form (input is assumed to be in non-SSA form)
     """
-    print(json.dumps(variable_renaming(phi_nodes(json.load(sys.stdin)))))
+    print(json.dumps(to_ssa_form(json.load(sys.stdin))))
 
 
 @main.command(name="out")
