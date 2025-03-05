810a99314520e9dd322ce7dd82184b9ade96feb1
Simon <simonbertron@gmail.com>
2025-03-05 10:50:57 -0500

    L6: first pass at local variable renaming
    It works on a very simple program so I'm going to move on to the phi
    nodes pass and do some end-to-end debugging

--- l6/ssa.py
+++ l6/ssa.py
@@ -10,8 +10,30 @@ import click
 
 
 @ut.local_optimization
-def variable_renaming(block):
-    return block
+def variable_renaming(instrs):
+    out = []
+    most_recent_name = {}
+    for instr in instrs:
+        new_instr = copy.deepcopy(instr)
+        if "args" in new_instr:
+            new_instr["args"] = [
+                (
+                    arg + "." + str(most_recent_name[arg])
+                    if arg in most_recent_name
+                    else arg
+                )
+                for arg in new_instr["args"]
+            ]
+        if "dest" in new_instr:
+            dest = new_instr["dest"]
+            if dest not in most_recent_name:
+                most_recent_name[dest] = 0
+            most_recent_name[dest] += 1
+            new_instr["dest"] = dest + "." + str(most_recent_name[dest])
+
+        out.append(new_instr)
+
+    return out
 
 
 @ut.global_optimization
@@ -30,7 +52,7 @@ def main():
 
 
 @main.command(name="in")
-def into_ssa_command(validate):
+def into_ssa_command():
     """
     Convert input program into SSA form (input is assumed to be in non-SSA form)
     """
@@ -38,7 +60,7 @@ def into_ssa_command(validate):
 
 
 @main.command(name="out")
-def out_of_ssa_command(validate):
+def out_of_ssa_command():
     """
     Convert input program out of SSA form (input is assumed to be in SSA form)
     """
