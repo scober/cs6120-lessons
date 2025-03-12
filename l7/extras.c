b0c30a2d9ed034ddebbd91106489d7458293df02
Simon <simonbertron@gmail.com>
2025-03-12 12:53:38 -0400

    L7: working LLVM pass that links in custom C code

---
+++ l7/extras.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void logint(int i) {
+  printf("test: %i\n", i);
+}
--- l7/llvm-pass-skeleton/skeleton/Skeleton.cpp
+++ l7/llvm-pass-skeleton/skeleton/Skeleton.cpp
@@ -9,32 +9,45 @@ using namespace llvm;
 namespace {
 
 struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
-    PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
-        for (auto &F : M) {
-          for (auto &B : F) {
-            for (auto &I : B) {
-              if (auto *BO = dyn_cast<BinaryOperator>(&I)) {
-                errs() << "op:\n";
-                BO->print(errs());
-                errs() << '\n';
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
+    for (auto &F : M) {
+      LLVMContext& Ctx = F.getContext();
 
-                IRBuilder<> builder(BO);
-                Value* lhs = BO->getOperand(0);
-                Value* rhs = BO->getOperand(1);
 
-                errs() << "lhs:\n    ";
-                lhs->print(errs());
-                errs() << '\n';
-                errs() << "rhs:\n    ";
-                rhs->print(errs());
-              }
-            }
+      std::vector<Type*> paramTypes = {Type::getInt32Ty(Ctx)};
+      Type *retType = Type::getVoidTy(Ctx);
+      FunctionType *logFuncType = FunctionType::get(retType, paramTypes, false);
+      FunctionCallee logFunc =
+          F.getParent()->getOrInsertFunction("logint", logFuncType);
+
+      for (auto &B : F) {
+        for (auto &I : B) {
+          if (auto *BO = dyn_cast<BinaryOperator>(&I)) {
+            errs() << "op:\n";
+            BO->print(errs());
+            errs() << '\n';
+
+            IRBuilder<> builder(BO);
+            builder.SetInsertPoint(&B, ++builder.GetInsertPoint());
+
+            Value* args[] = {BO};
+            builder.CreateCall(logFunc, args);
+            Value* lhs = BO->getOperand(0);
+            Value* rhs = BO->getOperand(1);
+
+            errs() << "lhs:\n    ";
+            lhs->print(errs());
+            errs() << '\n';
+            errs() << "rhs:\n    ";
+            rhs->print(errs());
           }
         }
-        errs() << '\n';
-        errs() << '\n';
-        return PreservedAnalyses::none();
-    };
+      }
+    }
+    errs() << '\n';
+    errs() << '\n';
+    return PreservedAnalyses::none();
+  };
 };
 
 }
