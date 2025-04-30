94c5b96c07cd9d13977a69aea76ee08bc3f54640
Simon <simonbertron@gmail.com>
2025-04-30 10:52:00 -0400

    PROJECT: conjunctive normal form
    Transforming all predicates to conjunctive normal form regularizes the
    AST and will hopefully simplify the later passes.

--- project/transform.py
+++ project/transform.py
@@ -86,40 +86,160 @@ def presburger_elimination_possibilities(tree):
     )
 
 
+def replace_subtree(tree, root, transform):
+    parent = find_parent(root, tree)
+    attr = None
+    for name, value in ast.iter_fields(parent):
+        if value == root:
+            attr = name
+    assert attr
+
+    transformed = transform(root)
+    setattr(parent, attr, transformed)
+    return transformed
+
+
 def remove_alls_from_subtree(tree, root):
+    new_root = root
     for node in ast.iter_child_nodes(root):
         remove_alls_from_subtree(tree, node)
     if is_quantifier(root) and root.func.id == "all":
-        parent = find_parent(root, tree)
-        attr = None
-        for name, value in ast.iter_fields(parent):
-            if value == root:
-                attr = name
-        assert attr
 
-        # DeMorgan's Law:
-        # all(predicate) == not any(not predicate)
-        predicate = root.args[0]
-        demorganized = ast.UnaryOp(
-            ast.Not(),
-            ast.Call(
-                ast.Name("any", ast.Load()), [ast.UnaryOp(ast.Not(), predicate)], []
-            ),
-        )
-        setattr(parent, attr, demorganized)
-    return tree, demorganized
+        def demorganize(node):
+            # DeMorgan's Law:
+            # all(predicate) == not any(not predicate)
+            predicate = node.args[0]
+            return ast.UnaryOp(
+                ast.Not(),
+                ast.Call(
+                    ast.Name("any", ast.Load()), [ast.UnaryOp(ast.Not(), predicate)], []
+                ),
+            )
+
+        new_root = replace_subtree(tree, root, demorganize)
+    return tree, new_root
+
+
+def conjunctivize(tree):
+    if type(tree) == ast.UnaryOp and type(tree.op) == ast.Not:
+        dnf = disjunctivize(tree.operand)
+        if type(dnf) == ast.BoolOp and type(dnf.op) == ast.And:
+            tree = ast.BoolOp(
+                ast.Or(),
+                [ast.UnaryOp(ast.Not(), conjunct) for conjunct in dnf.values],
+            )
+        elif type(dnf) == ast.BoolOp and type(dnf.op) == ast.Or:
+            tree = ast.BoolOp(
+                ast.And(),
+                [ast.UnaryOp(ast.Not(), disjunct) for disjunct in dnf.values],
+            )
+    elif type(tree) == ast.BoolOp and type(tree.op) == ast.Or:
+        pass
+    elif type(tree) == ast.BoolOp and type(tree.op) == ast.And:
+        # distributive law:
+        # p and (q or r) == (p and q) or (p and r)
+        left = conjunctivize(tree.values[0])
+        if len(tree.values) > 2:
+            right = conjunctivize(ast.BoolOp(ast.And(), tree.values[1:]))
+        else:
+            right = conjunctivize(tree.values[1])
+
+        if type(right) == ast.BoolOp and type(right.op) == ast.Or:
+            tree = ast.BoolOp(
+                ast.Or(),
+                [
+                    ast.BoolOp(ast.And(), [left] + [disjunct])
+                    for disjunct in right.values
+                ],
+            )
+        elif type(left) == ast.BoolOp and type(left.op) == ast.Or:
+            tree = ast.BoolOp(
+                ast.Or(),
+                [
+                    ast.BoolOp(ast.And(), [disjunct] + [right])
+                    for disjunct in left.values
+                ],
+            )
+    if isinstance(tree, ast.AST):
+        for name, value in ast.iter_fields(tree):
+            if type(value) == list:
+                setattr(tree, name, list(map(conjunctivize, value)))
+            else:
+                setattr(tree, name, conjunctivize(value))
+
+    return tree
+
+
+def disjunctivize(tree):
+    if type(tree) == ast.UnaryOp and type(tree.op) == ast.Not:
+        cnf = conjunctivize(tree.operand)
+        if type(cnf) == ast.BoolOp and type(cnf.op) == ast.Or:
+            tree = ast.BoolOp(
+                ast.And(),
+                [ast.UnaryOp(ast.Not(), disjunct) for disjunct in cnf.values],
+            )
+        elif type(cnf) == ast.BoolOp and type(cnf.op) == ast.And:
+            tree = ast.BoolOp(
+                ast.Or(),
+                [ast.UnaryOp(ast.Not(), conjunct) for conjunct in cnf.values],
+            )
+    elif type(tree) == ast.BoolOp and type(tree.op) == ast.Or:
+        # distributive law:
+        # p or (q and r) == (p or q) and (p or r)
+        left = disjunctivize(tree.values[0])
+        if len(tree.values) > 2:
+            right = disjunctivize(ast.BoolOp(ast.Or(), tree.values[1:]))
+        else:
+            right = disjunctivize(tree.values[1])
+
+        if type(right) == ast.BoolOp and type(right.op) == ast.And:
+            tree = ast.BoolOp(
+                ast.And(),
+                [
+                    ast.BoolOp(ast.Or(), [left] + [conjunct])
+                    for conjunct in right.values
+                ],
+            )
+        elif type(left) == ast.BoolOp and type(left.op) == ast.And:
+            tree = ast.BoolOp(
+                ast.And(),
+                [
+                    ast.BoolOp(ast.Or(), [conjunct] + [right])
+                    for conjunct in left.values
+                ],
+            )
+    elif type(tree) == ast.BoolOp and type(tree.op) == ast.And:
+        pass
+
+    if isinstance(tree, ast.AST):
+        for name, value in ast.iter_fields(tree):
+            if type(value) == list:
+                setattr(tree, name, list(map(disjunctivize, value)))
+            else:
+                setattr(tree, name, disjunctivize(value))
+
+    return tree
 
 
 def remove_alls(tree, roots):
     new_roots = set()
     for root in roots:
-        _, new_root = remove_alls_from_subtree(tree, root)
+        tree, new_root = remove_alls_from_subtree(tree, root)
         new_roots.add(new_root)
 
     return tree, new_roots
 
 
 def conjunctive_normal_form(tree, roots):
+    new_roots = set()
+    for root in roots:
+        new_root = replace_subtree(tree, root, conjunctivize)
+        new_roots.add(new_root)
+
+    return tree, new_roots
+
+
+def push_down_anys(tree, roots):
     return tree, roots
 
 
@@ -132,8 +252,10 @@ def do_presburger_elimination(tree):
 
     # removing instances of all regularizes the tree
     tree, roots = remove_alls(tree, candidates)
-    tree, roots = conjunctive_normal_form(tree, candidates)
-    tree, roots = eliminate_quantifiers(tree, candidates)
+    # as does putting each predicate into conjunctive normal form
+    tree, roots = conjunctive_normal_form(tree, roots)
+    tree, roots = push_down_anys(tree, roots)
+    tree, roots = eliminate_quantifiers(tree, roots)
 
     return tree
 
@@ -148,7 +270,6 @@ def cli():
 def transformation_pass_command(filename):
     path = Path(filename)
     with open(path, "r") as file:
-        # print(ast.dump(ast.parse(file.read()), indent=2))
         transformed = do_presburger_elimination(ast.parse(file.read()))
         transformed_path = path.with_stem(str(path.stem) + "_pa_qe")
         with open(transformed_path, "w") as transformed_file:
