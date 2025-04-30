33608ab3586148764ba3f0b3ad8f9dc56ed8e88a
Simon <simonbertron@gmail.com>
2025-04-30 10:51:23 -0400

    PROJECT: add new test cases
    These test cases are targeted at the conjunctive normal form
    transformation

--- project/test.py
+++ project/test.py
@@ -1,39 +1,51 @@
-def single_any(n):
-    return any(n % i == 0 for i in range(n))
+# def single_any(n):
+#    return any(n % i == 0 for i in range(n))
+#
+#
+# def single_all(n):
+#    return all(n % i == 0 for i in range(n))
+#
+#
+# def nested_all_all(n):
+#    return all(all(j % i == 0 for j in range(n)) for i in range(n))
+#
+#
+# def nested_any_any(n):
+#    return any(any(j % i == 0 for j in range(n)) for i in range(n))
+#
+#
+# def nested_any_all(n):
+#    return any(all(j % i == 0 for j in range(n)) for i in range(n))
+#
+#
+# def nested_all_any(n):
+#    return all(any(j % i == 0 for j in range(n)) for i in range(n))
+#
+#
+# def non_eliminatable(n):
+#    return any(n * n % i == 0 for i in range(n))
+#
+#
+# def disjunction_one(n):
+#    return any(n % i == 0 or n % i == 1 or n % i == 2 for i in range(n + n))
+#
+#
+# def disjunctive_normal_form(n):
+#    return any(
+#        (n % i == 0 or n + n % i == 0) and (n % i == 1 or n + n % i == 1)
+#        for i in range(n + n)
+#    )
 
 
-def single_all(n):
-    return all(n % i == 0 for i in range(n))
-
-
-def nested_all_all(n):
-    return all(all(j % i == 0 for j in range(n)) for i in range(n))
-
-
-def nested_any_any(n):
-    return any(any(j % i == 0 for j in range(n)) for i in range(n))
-
-
-def nested_any_all(n):
-    return any(all(j % i == 0 for j in range(n)) for i in range(n))
-
-
-def nested_all_any(n):
-    return all(any(j % i == 0 for j in range(n)) for i in range(n))
-
-
-def non_eliminatable(n):
-    return any(n * n % i == 0 for i in range(n))
+def negated_disjunctive_normal_form(n):
+    return any(
+        not ((n % i == 0 or n + n % i == 0) and (n % i == 1 or n + n % i == 1))
+        for i in range(n + n)
+    )
 
 
 def main():
-    print(single_any(6))
-    print(single_all(6))
-    print(nested_all_all(6))
-    print(nested_any_any(6))
-    print(nested_any_all(6))
-    print(nested_all_any(6))
-    print(non_eliminatable(6))
+    pass
 
 
 if __name__ == "__main__":
--- project/test_pa_qe.py
+++ project/test_pa_qe.py
@@ -1,31 +1,17 @@
-def single_any(n):
-    return any((n % i == 0 for i in range(n)))
+def negated_disjunctive_normal_form(n):
+    return any(
+        (
+            not n % i == 0
+            and (not n + n % i == 0)
+            or (not n % i == 1 and (not n + n % i == 1))
+            for i in range(n + n)
+        )
+    )
 
-def single_all(n):
-    return not any(not (n % i == 0 for i in range(n)))
-
-def nested_all_all(n):
-    return not any(not (not any(not (j % i == 0 for j in range(n))) for i in range(n)))
-
-def nested_any_any(n):
-    return any((any((j % i == 0 for j in range(n))) for i in range(n)))
-
-def nested_any_all(n):
-    return any((not any(not (j % i == 0 for j in range(n))) for i in range(n)))
-
-def nested_all_any(n):
-    return not any(not (any((j % i == 0 for j in range(n))) for i in range(n)))
-
-def non_eliminatable(n):
-    return any((n * n % i == 0 for i in range(n)))
 
 def main():
-    print(single_any(6))
-    print(single_all(6))
-    print(nested_all_all(6))
-    print(nested_any_any(6))
-    print(nested_any_all(6))
-    print(nested_all_any(6))
-    print(non_eliminatable(6))
-if __name__ == '__main__':
+    pass
+
+
+if __name__ == "__main__":
     main()
