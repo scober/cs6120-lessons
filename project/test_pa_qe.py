9e11f2c81fe7d7a1309de2384673f451b414a08b
Simon <simonbertron@gmail.com>
2025-05-07 11:23:19 -0400

    PROJECT: up-to-date hand-testing file

--- project/test_pa_qe.py
+++ project/test_pa_qe.py
@@ -1,15 +1,15 @@
 def simple_one(n):
-    return -2 - max(2 * (2 * n), 22) > 2 or -2 % 2 < max(2 * (2 * n), 22) % 2
+    return min(2 * (2 * n), 22) - -2 > 2 or -2 % 2 < min(2 * (2 * n), 22) % 2
 
 
 def simple_two(n):
-    return not (min(-2, 21) - 2 * (2 * n) > 2 or min(-2, 21) % 2 < 2 * (2 * n) % 2)
+    return not (2 * (2 * n) - max(-2, 21) > 2 or max(-2, 21) % 2 < 2 * (2 * n) % 2)
 
 
 def nested(n):
     return all(
         (
-            -2 - max(22, 2 * (n + j)) > 2 or -2 % 2 < max(22, 2 * (n + j)) % 2
+            min(22, 2 * (n + j)) - -2 > 2 or -2 % 2 < min(22, 2 * (n + j)) % 2
             for j in range(n + n)
         )
     )
@@ -17,22 +17,22 @@ def nested(n):
 
 def disjunctive_normal_form(n):
     return (
-        (min(-1, 0) - max(2 * n, n) > 1 or min(-1, 0) % 1 < max(2 * n, n) % 1)
+        (min(2 * n, n) - max(-1, 0) > 1 or max(-1, 0) % 1 < min(2 * n, n) % 1)
         or (
-            -2 - max(2 * (2 * n), 2 * n, 34) > 2
-            or -2 % 2 < max(2 * (2 * n), 2 * n, 34) % 2
+            min(2 * (2 * n), 2 * n, 34) - -2 > 2
+            or -2 % 2 < min(2 * (2 * n), 2 * n, 34) % 2
         )
     ) or (
-        (min(n, -2, 0) - 2 * (2 * n) > 2 or min(n, -2, 0) % 2 < 2 * (2 * n) % 2)
+        (2 * (2 * n) - max(n, -2, 0) > 2 or max(n, -2, 0) % 2 < 2 * (2 * n) % 2)
         or (
-            min(n, -2) - max(34, 2 * (2 * n)) > 2
-            or min(n, -2) % 2 < max(34, 2 * (2 * n)) % 2
+            min(2 * (2 * n), 34) - max(n, -2) > 2
+            or max(n, -2) % 2 < min(2 * (2 * n), 34) % 2
         )
     )
 
 
 def negation(n):
-    return min(4, -2, 21) - 2 * (2 * n) > 2 or min(4, -2, 21) % 2 < 2 * (2 * n) % 2
+    return 2 * (2 * n) - max(21, 4, -2) > 2 or max(21, 4, -2) % 2 < 2 * (2 * n) % 2
 
 
 def equality(n):
@@ -41,18 +41,18 @@ def equality(n):
 
 def inequality(n):
     return (
-        (-2 - max(2 * (2 * n), 22) > 2 or -2 % 2 < max(2 * (2 * n), 22) % 2)
-        or (min(22, -2) - 2 * (2 * n) > 2 or min(22, -2) % 2 < 2 * (2 * n) % 2)
+        (min(22, 2 * (2 * n)) - -2 > 2 or -2 % 2 < min(22, 2 * (2 * n)) % 2)
+        or (2 * (2 * n) - max(22, -2) > 2 or max(22, -2) % 2 < 2 * (2 * n) % 2)
     ) or (
-        (-1 - max(3, 2 * n) > 1 or -1 % 1 < max(3, 2 * n) % 1)
-        or (min(-1, 3) - 2 * n > 1 or min(-1, 3) % 1 < 2 * n % 1)
+        (min(3, 2 * n) - -1 > 1 or -1 % 1 < min(3, 2 * n) % 1)
+        or (2 * n - max(-1, 3) > 1 or max(-1, 3) % 1 < 2 * n % 1)
     )
 
 
 def vars_on_both_sides(n):
     return (
-        min(-3, 2 * n) - max(3 * (2 * n), 12) > 3
-        or min(-3, 2 * n) % 3 < max(3 * (2 * n), 12) % 3
+        min(3 * (2 * n), 12) - max(2 * n, -3) > 3
+        or max(2 * n, -3) % 3 < min(3 * (2 * n), 12) % 3
     )
 
 
